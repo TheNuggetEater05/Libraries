bdd6a9b4c86ef12c4f65029c38794c3fc6696aca
Jaydenn <63414528+TheNuggetEater05@users.noreply.github.com>
2023-08-15 13:46:50 -0400

    Add drawing function to esp.lua

--- esp.lua
+++ esp.lua
@@ -1,1 +1,17 @@
+getgenv().esp_drawings = {}
 
+local drawing_new = Drawing.new or ""
+
+local lib = {}
+
+local funcs = {} do
+  function funcs:draw_new(type, add_to_table, props)
+    local properties = props or {}
+    local drawing = drawing_new(type)
+    for index, property in pairs(properties) do
+      drawing[index] = property;
+    end
+  end
+end
+
+return lib;
